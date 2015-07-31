d167bf90e8d65ee448f70dab40d2929a796244e1
unknown <ksjdragon@gmail.com>
2015-07-31 10:02:08 -0400

    removed buildtest compatibility

--- init.lua
+++ init.lua
@@ -5,14 +5,4 @@ dofile(minetest.get_modpath("equivalent_exchange").."/condenser.lua")
 dofile(minetest.get_modpath("equivalent_exchange").."/covalence_dust.lua")
 dofile(minetest.get_modpath("equivalent_exchange").."/energy_collector.lua")
 
-if minetest.get_modpath("buildtest")~=nil then
-	buildtest.canPumpInto["equivalent_exchange:condenser"] = {
-		"from",
-		on_send = function(pos)
-			equivalent_exchange.convert(pos)
-		end	
-	}
-	buildtest.pumps.pulls["equivalent_exchange:condenser"] = {"from"}
-end
-
 print("[equivalent_exchange] Mod loaded!")
